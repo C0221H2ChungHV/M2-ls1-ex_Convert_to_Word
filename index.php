548c334216d26772a974a7d3f3ae72e73e7b3ac7
C0221H2ChungHV <79875953+C0221H2ChungHV@users.noreply.github.com>
2021-04-10 10:36:33 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,33 @@
+<?php ?>
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <style>
+        input[type=text] {
+            width: 300px;
+            font-size: 16px;
+            border: 2px solid #ccc;
+            border-radius: 4px;
+            padding: 12px 10px 12px 10px;
+        }
+        #submit {
+            border-radius: 2px;
+            padding: 10px 32px;
+            font-size: 16px;
+        }
+    </style>
+</head>
+<body>
+<h2>Convert to words</h2>
+<form method="post">
+    <input type="text" name="search" placeholder="Nhập số cần đọc"/>
+    <input type = "submit" id = "submit" value = "Đọc"/>
+</form>
+</body>
+</html>
